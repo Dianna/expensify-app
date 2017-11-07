125dae58a7291267e1fc77b7e84a086cc655f9a8
Dianna <diannafaulk@gmail.com>
2017-11-07 15:42:16 -0800

    Filter expenses before computing

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -1,27 +1,29 @@
 import React from "react";
 import { connect } from "react-redux";
 import numeral from "numeral";
+import selectExpenses from "../selectors/expenses";
 import selectExpensesTotal from "../selectors/expenses-total";
 
-export const ExpensesSummary = props => {
-  const { expenses } = props;
+export const ExpensesSummary = ({ expenses }) => {
   const multipleExpenses = expenses.length > 1;
   const total = selectExpensesTotal(expenses);
   const formattedTotal = numeral(total / 100).format("$0,0.00");
+  const expenseWord = multipleExpenses ? "expenses" : "expense";
   return (
     <div>
       <p>
-        Viewing {expenses.length} {multipleExpenses
-          ? "expenses"
-          : "expense"}{" "}
-        totalling {formattedTotal}
+        Viewing {expenses.length} {expenseWord} totalling {formattedTotal}
       </p>
     </div>
   );
 };
 
-const mapStateToProps = state => ({
-  expenses: state.expenses
-});
+const mapStateToProps = state => {
+  const visibleExpenses = selectExpenses(state.expenses, state.filters);
+  return {
+    expenses: visibleExpenses
+  };
+  // expenses: state.expenses
+};
 
 export default connect(mapStateToProps)(ExpensesSummary);
