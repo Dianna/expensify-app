fa5bd87db35d490b327d2b66e6942241da145912
Dianna <diannafaulk@gmail.com>
2017-11-10 16:47:39 -0800

    Add private routes

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -6,12 +6,12 @@ import ExpenseForm from "./ExpenseForm";
 export class EditExpensePage extends React.Component {
   onSubmit = expense => {
     this.props.startEditExpense(this.props.expense.id, expense);
-    this.props.history.push("/");
+    this.props.history.push("/dashboard");
   };
 
   startRemoveExpense = () => {
     this.props.startRemoveExpense(this.props.expense.id);
-    this.props.history.push("/");
+    this.props.history.push("/dashboard");
   };
 
   render() {
--- src/tests/components/EditExpensePage.test.js
+++ src/tests/components/EditExpensePage.test.js
@@ -26,12 +26,12 @@ test("should render EditExpensePage correctly", () => {
 
 test("should handle startEditExpense", () => {
   wrapper.find("ExpenseForm").prop("onSubmit")(expense);
-  expect(history.push).toHaveBeenLastCalledWith("/");
+  expect(history.push).toHaveBeenLastCalledWith("/dashboard");
   expect(startEditExpense).toHaveBeenLastCalledWith(expense.id, expense);
 });
 
 test("should handle startRemoveExpense", () => {
   wrapper.find("button").simulate("click");
-  expect(history.push).toHaveBeenLastCalledWith("/");
+  expect(history.push).toHaveBeenLastCalledWith("/dashboard");
   expect(startRemoveExpense).toHaveBeenLastCalledWith(expense.id);
 });
